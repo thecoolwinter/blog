7833850c0dbab19aa428b801fbd0c4f9d44f5991
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2024-11-03 14:05:14 -0600

    Fix Sitemap Again

--- Sources/Jobs/SitemapTxtJob.swift
+++ Sources/Jobs/SitemapTxtJob.swift
@@ -5,7 +5,7 @@ struct SitemapTxtJob: Job {
             "",
             "about"
         ] + JobHelpers.getAllPosts(postsDir: postsDir).map({
-            $0.url.deletingPathExtension().path()
+            $0.url.deletingLastPathComponent().path()
         })
 
         let contents = sites.map({ "https://khanwinter.com/" + $0 }).joined(separator: "\n")
