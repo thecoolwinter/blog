8766664d8e52c4a51206a8376fc36a8456dd0dda
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2026-01-07 14:05:48 -0600

    Deploy 2026-01-07

--- Sources/Extensions/Date+Format.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension Date {
-    var year: String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy"
-        return formatter.string(from: self)
-    }
-}
